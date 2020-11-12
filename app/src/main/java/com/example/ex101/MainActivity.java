36abb75c4582dbcff2808910b815808029d3ae92
OriOfek <oriofek106@gmail.com>
2020-11-12 12:52:58 +0200

    finish

--- app/src/main/java/com/example/ex101/MainActivity.java
+++ app/src/main/java/com/example/ex101/MainActivity.java
@@ -11,10 +11,13 @@ import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout screen;
     AlertDialog.Builder builder;
+    Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,13 +99,66 @@ public class MainActivity extends AppCompatActivity {
         builder.setIcon(R.drawable.untitled);
 
 
-        builder.setPositiveButton("gen", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("gen", new DialogInterface.OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialog,
                                 int which)
             {
-                screen.setBackground();
+                rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                screen.setBackgroundColor(color);
+            }
+        });
+
+        builder.setPositiveButton("exit", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog,
+                                int which)
+            {
+
+                // When the user click yes button
+                // then app will close
+                dialog.dismiss();
+            }
+        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+
+        // Show the Alert Dialog box
+        alertDialog.show();
+    }
+
+    public void sit5(View view) {
+        // Create the object of
+        // AlertDialog Builder class
+        builder = new AlertDialog.Builder(MainActivity.this);
+
+        // Set Alert Title
+        builder.setTitle("Alert !");
+        builder.setIcon(R.drawable.untitled);
+
+
+        builder.setNegativeButton("generate", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog,
+                                int which)
+            {
+                rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                screen.setBackgroundColor(color);
+            }
+        });
+        builder.setNeutralButton("reset", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog,
+                                int which)
+            {
+                screen.setBackgroundColor(Color.WHITE);
             }
         });
 
