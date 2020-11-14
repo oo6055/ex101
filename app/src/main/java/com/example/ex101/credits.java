efeba304886c2a87ef5bd05b9e53e48cc1f1d863
OriOfek <oriofek106@gmail.com>
2020-11-14 23:30:15 +0200

    need to write comments

--- app/src/main/java/com/example/ex101/credits.java
+++ app/src/main/java/com/example/ex101/credits.java
@@ -2,7 +2,10 @@ package com.example.ex101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class credits extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class credits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menu.add("Main activity");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return  true;
     }
 }
